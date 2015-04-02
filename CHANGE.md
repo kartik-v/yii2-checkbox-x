9e2918ce8f7157ba8866d5d526a73c778616cc98
Kartik Visweswaran <kartikv2@gmail.com>
2015-04-02 23:28:09 +0530

    Updates to release v1.2.0 fixes #8 & fixes #9

--- CHANGE.md
+++ CHANGE.md
@@ -3,7 +3,9 @@ version 1.2.0
 **Date**: 01-Apr-2015
 
 - Code formatting updates as per Yii2 coding style.
-- (enh #7): New initInputType property to set input type for initializing bootstrap-checkbox-x plugin.
+- (enh #7): New `initInputType` property to set input type for initializing bootstrap-checkbox-x plugin.
+- (enh #8): New `autoLabel` property to allow automatic configuration of label text, styles, and positions.
+- (enh #9): New `labelSettings` property to allow configuring checkbox labels better.
 
 version 1.1.0
 =============
--- CheckboxX.php
+++ CheckboxX.php
@@ -10,7 +10,9 @@
 namespace kartik\checkbox;
 
 use Yii;
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
 
 /**
  * An extended checkbox widget for Yii Framework 2 based on the bootstrap-checkbox-x plugin
@@ -25,20 +27,51 @@ class CheckboxX extends \kartik\base\InputWidget
 {
     const INPUT_TEXT = 'textInput';
     const INPUT_CHECKBOX = 'checkbox';
+    const LABEL_LEFT = 'left';
+    const LABEL_RIGHT = 'right';
 
     /**
      * @var string initialization input type. Note, the widget by default
      * uses a text input to initialize the plugin instead of checkbox for
      * better label styling, alignment and using templates within ActiveField.
-     * Can be one of CheckboxX::INPUT_TEXT or CheckboxX::INPUT_CHECKBOX.
+     * Can be one of `CheckboxX::INPUT_TEXT` or `CheckboxX::INPUT_CHECKBOX`.
      */
     public $initInputType = self::INPUT_TEXT;
-    
+
+    /**
+     * @var bool automatically generate, style, and position labels with respect to
+     * the checkbox x. If this is `true`, the labels will automatically be positioned
+     * and styled based on label settings. When this is set to `true` and you have set
+     * the `model` and `attribute`, the label will be automatically generated. If you are
+     * not using this with a model, then  you must set the property `labelSettings['label']`
+     * for automatic label styling to work.
+     * NOTE:
+     * If this is `true`, and you are using the widget within yii ActiveField, then
+     * you must disable the active field label generation to avoid duplicate labels.
+     * For example:
+     * ```
+     * $form->field($model, 'attr')->widget(CheckboxX::classname(), [
+     *      'autoLabel'=>true
+     * ])->label(false);
+     * ```
+     */
+    public $autoLabel = false;
+
+    /**
+     * @var the settings for the label. The following properties are recognized
+     * - `label`: string, the label to be used. When using with model, this will be automatically generated
+     *   if not set.
+     * - `position`: string, the position of the label with respect to the checkbox. Must be one of
+     *   `CheckboxX::LABEL_LEFT` or `CheckboxX::LABEL_RIGHT`. Defaults to `CheckboxX::LABEL_RIGHT` if not set.
+     * - `options`: array, the HTML attributes for the label.
+     */
+    public $labelSettings = [];
+
     /**
      * @inheritdoc
      */
     protected $_pluginName = 'checkboxX';
-    
+
     /**
      * @inheritdoc
      */
@@ -49,7 +82,39 @@ class CheckboxX extends \kartik\base\InputWidget
         if ($this->pluginLoading) {
             Html::addCssClass($this->options, 'cbx-loading');
         }
-        echo $this->getInput($this->initInputType);
+        if ($this->initInputType !== self::INPUT_TEXT && $this->initInputType !== self::INPUT_CHECKBOX) {
+            throw new InvalidConfigException('The "initInputType" property must be one of "' . self::INPUT_TEXT . '" or "' . self::INPUT_CHECKBOX . '"');
+        }
+        $this->initMarkup();
+    }
+
+    /**
+     * Initializes markup & styling for checkbox, label, and parses label positions
+     */
+    public function initMarkup()
+    {
+        if (!$this->autoLabel || (!$this->hasModel() && empty($this->labelSettings['label']))) {
+            echo $this->getInput($this->initInputType);
+            return;
+        }
+        $label = ArrayHelper::getValue($this->labelSettings, 'label', '');
+        $options = ArrayHelper::getValue($this->labelSettings, 'options', []);
+        $position = ArrayHelper::getValue($this->labelSettings, 'position', self::LABEL_RIGHT);
+        Html::addCssClass($options, 'cbx-label');
+        if ($this->disabled || $this->readonly ||
+            ArrayHelper::getValue($this->options, 'disabled', false) ||
+            ArrayHelper::getValue($this->options, 'readonly', false)
+        ) {
+            Html::addCssClass($options, 'disabled');
+        }
+        $label = $this->hasModel() ?
+            Html::activeLabel($this->model, $this->attribute, $options) :
+            Html::label($label, $this->options['id'], $options);
+        if ($this->initInputType !== self::INPUT_TEXT) {
+            $this->options['label'] = '';
+        }
+        $input = $this->getInput($this->initInputType);
+        echo ($position === self::LABEL_RIGHT) ? $input . $label : $label . $input;
     }
 
     /**
